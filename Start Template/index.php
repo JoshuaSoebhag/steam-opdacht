198e2f462a290b05abee3e945987a07761ac0c3d
JoshuaSoebhag <302663342@student.rocmondriaan.nl>
2025-03-11 12:55:27 +0100

    stuk gemaakt

---
+++ Start Template/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Steam Profile Page</title>
+    <link rel="stylesheet" href="/HTML combinatie PHP opdrachten/Start Template/css/style.css">
+</head>
+<body>
+    <div class="wrapper">
+    <header class="header">
+        <img src="/HTML combinatie PHP opdrachten/Start Template/img/logo_steam.svg" alt="steam-logo">
+        <nav class="navigation">
+            <ul>
+                <li><a href="store"></a></li>
+                <li><a href="community"></a></li>
+                <li><a href="Joshua"></a></li>
+                <li><a href="chat"></a></li>
+                <li><a href="support"></a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <div class="profile">
+            <img src="img/pf.jpg" alt="profile-pic">
+            
+        </div>
+
+    </main>
+    </div>
+</body>
+</html>
